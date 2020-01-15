08ef81b3f62b5713f7e83fcbfd4ab2fc71e2fd5a
John J. Martínez <jmartinez@ecs-la.com>
2020-01-15 14:54:02 -0500

    Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxXml2jsonService } from 'ngx-xml2json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fccd';
+
+  textInXml: string;
+  servers: any;
+  columns: any = [];
+
+  constructor(private ngxXml2jsonService: NgxXml2jsonService) {
+  }
+
+  convert() {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(this.textInXml, 'text/xml');
+    const obj = this.ngxXml2jsonService.xmlToJson(xml);
+    this.servers = obj['FileZilla3']['Servers']['Server'].map((server) => {
+      return {
+        name: server.Name, 
+        host: server.Host, 
+        port: server.Port,
+        user: server.User,
+        pass : atob(server.Pass),
+        pasvMode: server.PasvMode
+      }
+    });
+    this.columns = Object.keys(this.servers[0]);
+  }
+
 }
